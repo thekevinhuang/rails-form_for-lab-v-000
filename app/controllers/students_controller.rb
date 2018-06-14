58e3c80b09bd61ee943bd9ccaf474b3d5164794b
Kevin Huang <yellow.khaos@gmail.com>
2018-06-14 02:27:09 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -20,6 +20,7 @@ class StudentsController < ApplicationController
   def update
     @student = Student.find(params[:id])
     @student.update(post_params(:first_name, :last_name))
+    redirect_to student_path(@student)
   end
 
   private
