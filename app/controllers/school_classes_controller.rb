583140fefb663b64fbea94678221f22507caed38
Kevin Huang <yellow.khaos@gmail.com>
2018-06-14 01:23:50 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,22 +1,28 @@
 class SchoolClassesController <ApplicationController
 
-  def new
-
-  end
-
-  def create
-
-  end
-
   def show
     @school_class = SchoolClass.find(params[:id])
   end
 
+  def new
+    
+  end
+
+  def create
+    @school_class = SchoolClass.create(post)
+  end
+  
   def edit
     @school_class = SchoolClass.find(params[:id])
   end
 
   def update
 
+  end
+
+  private
+
+  def post_params(*args)
+    params.require(:school_class).permit(*args)
   end
 end
